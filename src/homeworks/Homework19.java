0c6f2a83500c08f87d6ad18b1a3197a1808f72a8
TaylorSimvnee <Mstaylorg98@gmail.com>
2022-06-04 18:56:27 -0700

    added a solution

--- src/homeworks/Homework19.java
+++ src/homeworks/Homework19.java
@@ -3,6 +3,7 @@ package homeworks;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.TreeSet;
 
 
 public class Homework19 {
@@ -107,6 +108,22 @@ public class Homework19 {
 
             return new String[]{letters, digits, specials};
         }
+           /**
+    Write a method that takes an int array argument and returns the second max value as an int
+    NOTE: Assume length of the array is always more than one
+    TEST DATA:
+    [-5, 4, 1, 10, 2]
+    EXPECTED OUTPUT:
+    4
+     */
+
+           public static int secondMax(int[] arr){
+               TreeSet<Integer> numbers = new TreeSet<>();
+               for (Integer number : arr) {
+                   numbers.add(number);
+               }
+               return new ArrayList<>(numbers).get(numbers.size()-2);
+           }
 
     public static void main(String[] args) {
 
